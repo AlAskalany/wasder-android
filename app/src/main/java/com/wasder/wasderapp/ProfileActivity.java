0504406cbd83b977048d8ff18c4dc54a32624123
Ahmed Alaskalany <ahmed.alaskalany@gmail.com>
2017-09-06 01:23:15 +0200

    profile back button

--- app/src/main/java/com/wasder/wasderapp/ProfileActivity.java
+++ app/src/main/java/com/wasder/wasderapp/ProfileActivity.java
@@ -1,7 +1,13 @@
 package com.wasder.wasderapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
 	
@@ -10,5 +16,27 @@ public class ProfileActivity extends AppCompatActivity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_profile);
+		toolbar.setTitleTextColor(Color.WHITE);
+		setSupportActionBar(toolbar);
+		ActionBar actionBar = getSupportActionBar();
+		CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingtoolbar_profile);
+		collapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		int id = item.getItemId();
+		switch (id) {
+			case R.id.homeAsUp:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
