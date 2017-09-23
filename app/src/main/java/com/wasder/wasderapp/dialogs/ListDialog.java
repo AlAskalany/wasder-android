58ecc4329ddda49504cad8cfdeb3111441b00616
Ahmed AlAskalany <askalany@wasder.com>
2017-09-23 19:37:45 +0200

    purchase dialog

--- app/src/main/java/com/wasder/wasderapp/dialogs/ListDialog.java
+++ app/src/main/java/com/wasder/wasderapp/dialogs/ListDialog.java
@@ -3,6 +3,8 @@ package com.wasder.wasderapp.dialogs;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
 
 import com.wasder.wasderapp.R;
 
@@ -12,17 +14,31 @@ import com.wasder.wasderapp.R;
  */
 
 public class ListDialog extends Dialog {
-	
+
 	public ListDialog(@NonNull Context context) {
 		super(context);
 		setContentView(R.layout.dialog);
+		Button dialogConfirmButton = findViewById(R.id.dialog_confirm_button);
+		dialogConfirmButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				hide();
+			}
+		});
+		Button dialogCancelButton = findViewById(R.id.dialog_cancel_button);
+		dialogCancelButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				hide();
+			}
+		});
 	}
-	
-	public static ListDialog newInstance(Context context){
+
+	public static ListDialog newInstance(Context context) {
 		return new ListDialog(context);
 	}
-	
-	public void ShowListDialog(Context context){
+
+	public void ShowListDialog(Context context) {
 		this.show();
 	}
 }
--- app/src/main/java/com/wasder/wasderapp/ui/home/FeedRecyclerAdapter.java
+++ app/src/main/java/com/wasder/wasderapp/ui/home/FeedRecyclerAdapter.java
@@ -3,7 +3,11 @@ package com.wasder.wasderapp.ui.home;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -13,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.wasder.wasderapp.Interfaces.OnFragmentInteractionListener;
 import com.wasder.wasderapp.R;
 import com.wasder.wasderapp.Templates.BaseRecyclerAdapter;
+import com.wasder.wasderapp.dialogs.ListDialog;
 import com.wasder.wasderapp.models.FeedItem;
 import com.wasder.wasderapp.util.Helpers;
 
@@ -23,51 +28,73 @@ import com.wasder.wasderapp.util.Helpers;
 
 public class FeedRecyclerAdapter
 		extends BaseRecyclerAdapter<FeedItem, FeedRecyclerAdapter.FeedViewHolder> {
-	
+
 	public FeedRecyclerAdapter(Context context, LinearLayoutManager feedLinearLayoutManager, OnFragmentInteractionListener mListener) {
-		
+
 		super(FeedItem.class, R.layout.feed_item,
-		      FeedViewHolder.class,
-		      FirebaseDatabase.getInstance()
-		                      .getReference()
-		                      .child("feed"), "FeedRecyclerAdapter",
-		      context,
-		      mListener,
-		      feedLinearLayoutManager);
-		
+				FeedViewHolder.class,
+				FirebaseDatabase.getInstance().getReference().child("feed"), "FeedRecyclerAdapter",
+				context,
+				mListener,
+				feedLinearLayoutManager);
+
 	}
-	
+
 	@Override
 	protected void populateViewHolder(final FeedViewHolder viewHolder, final FeedItem feedItem, int position) {
-		
+
 		viewHolder.detailsImageButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
-			public void onClick(View view) {
-				
-				Intent intent = new Intent(viewHolder.itemView.getContext(), FeedDetailsActivity.class);
-				intent.putExtra("data_item", feedItem);
-				viewHolder.itemView.getContext()
-				                   .startActivity(intent);
+			public void onClick(final View view) {
+				PopupMenu feedPopupMenu = new PopupMenu(view.getContext(), view, Gravity.RIGHT);
+				MenuInflater inflater = feedPopupMenu.getMenuInflater();
+				inflater.inflate(R.menu.feed_menu, feedPopupMenu.getMenu());
+				feedPopupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+					@Override
+					public boolean onMenuItemClick(MenuItem item) {
+						int id = item.getItemId();
+						switch (id) {
+							case R.id.action_feed_bookmark:
+								return true;
+							case R.id.action_feed_register:
+								ListDialog listDialog = new ListDialog(view.getContext());
+								listDialog.show();
+
+								return true;
+							case R.id.action_feed_set_reminder:
+								return true;
+							default:
+						}
+						return false;
+					}
+				});
+				feedPopupMenu.show();
 				mListener.onFragmentInteractionListener("FeedFragment", FeedDetailsActivity.class, "Details");
 			}
 		});
-		
+
 		viewHolder.shareImageButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View view) {
-				
 				mListener.onFragmentInteractionListener("FeedFragment", viewHolder.feedItem, "Share");
 			}
 		});
-		
+
 		viewHolder.photoImageButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View view) {
-				
 				mListener.onFragmentInteractionListener("FeedFragment", viewHolder.feedItem, "Profile");
+			}
+		});
+		viewHolder.titleTextView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(view.getContext(), FeedDetailsActivity.class);
+				intent.putExtra("data_item", feedItem);
+				view.getContext().startActivity(intent);
 			}
 		});
 		viewHolder.titleTextView.setText(feedItem.getTitle());
@@ -78,10 +105,10 @@ public class FeedRecyclerAdapter
 		Helpers.Firebase.DownloadUrlImage(photoUrl, viewHolder.photoImageButton, true, R.drawable.avatar);
 		viewHolder.supplementaryTextView.setText(feedItem.getSupplementaryText());
 	}
-	
+
 	public static class FeedViewHolder
 			extends RecyclerView.ViewHolder {
-		
+
 		View mview;
 		TextView titleTextView;
 		TextView subheadTextView;
@@ -94,9 +121,9 @@ public class FeedRecyclerAdapter
 		ImageButton shareImageButton;
 		ImageButton detailsImageButton;
 		FeedItem feedItem;
-		
+
 		public FeedViewHolder(View itemView) {
-			
+
 			super(itemView);
 			mview = itemView;
 			titleTextView = itemView.findViewById(R.id.feed_card_header);
